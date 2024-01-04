7ed2ac46f91def2963c5f60ef70ceecdc73324a8
genexusbot <builder@genexus.com>
2024-01-04 15:57:20 -0300

    Package version: 1.4.0-beta.12

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
 			targets: ["GXAudioUIWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXAudioBL.git", exact: "1.4.0-beta.11"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.4.0-beta.11")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXAudioBL.git", exact: "1.4.0-beta.12"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.4.0-beta.12")
 	],
 	targets: [
 		.target(name: "GXAudioUIWrapper",
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXAudioUI",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXAudioUI-1.4.0-beta.11.xcframework.zip",
-			checksum: "9ec294bb4a7e620166541333ff43f29d045d1f92d2002b3f90695b0e1ed6c84a"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXAudioUI-1.4.0-beta.12.xcframework.zip",
+			checksum: "e8e800bab48cfd45c79bb94d999e8d1b8cd9d24c34c4e0003a7ed95ae3cf3571"
 		)
 	]
 )
