098837353dab529960787ee15ef3812c0fbf8068
Build Er <builder@macmini6.local>
2024-06-06 10:08:48 -0300

    Package version: 2.0.0-rc.34

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
 			targets: ["GXAudioUIWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXAudioBL.git", exact: "2.0.0-rc.32"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.0.0-rc.32")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXAudioBL.git", exact: "2.0.0-rc.34"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.0.0-rc.34")
 	],
 	targets: [
 		.target(name: "GXAudioUIWrapper",
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXAudioUI",
-			url: "https://pkgs.genexus.dev/iOS/preview/GXAudioUI-2.0.0-rc.32.xcframework.zip",
-			checksum: "3763d68d963e70f6f3fa929b85a982be6f92381e2cedad832bfbf817fc91a6bf"
+			url: "https://pkgs.genexus.dev/iOS/preview/GXAudioUI-2.0.0-rc.34.xcframework.zip",
+			checksum: "0f48d8e718ea24a602a8a213f320c6c4e9d1159865da3d96370f55925be4918b"
 		)
 	]
 )
