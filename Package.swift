5181ea727ec811ac8e01ffaea71c161913651925
The iOS Team @ GeneXus <builder@genexus.com>
2025-01-30 01:11:34 -0300

    Package version: 3.0.0-rc.15

--- Package.swift
+++ Package.swift
@@ -3,15 +3,15 @@ import PackageDescription
 
 let package = Package(
 	name: "GXAudioUI",
-	platforms: [.iOS("13.0"), .visionOS("1.0")],
+	platforms: [.iOS("15.0"), .visionOS("2.0")],
 	products: [
 		.library(
 			name: "GXAudioUI",
 			targets: ["GXAudioUIWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXAudioBL.git", exact: "3.0.0-rc.8"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "3.0.0-rc.8")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXAudioBL.git", exact: "3.0.0-rc.15"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "3.0.0-rc.15")
 	],
 	targets: [
 		.target(name: "GXAudioUIWrapper",
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXAudioUI",
-			url: "https://pkgs.genexus.dev/iOS/preview/GXAudioUI-3.0.0-rc.8.xcframework.zip",
-			checksum: "5ec04684020bb527ce7e77b2bd2cdfe43883b60752bbe12f682b306be7eca8ae"
+			url: "https://pkgs.genexus.dev/iOS/preview/GXAudioUI-3.0.0-rc.15.xcframework.zip",
+			checksum: "2eae73a25852e1e45f526baff4bb87ecc8c56661d30a78324fbaa1b9f658a393"
 		)
 	]
 )
