1a231ae5bf58f206a123f822c37aae3ad5d2fa31
The iOS Team @ GeneXus <builder@genexus.com>
2026-08-07 03:45:06 -0400

    Package version: 4.2.0

--- Package.swift
+++ Package.swift
@@ -1,30 +1,30 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
 	name: "GXAudioUI",
-	platforms: [.iOS("12.0")],
+	platforms: [.iOS("15.0"), .visionOS("2.0")],
 	products: [
 		.library(
 			name: "GXAudioUI",
 			targets: ["GXAudioUIWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXAudioBL.git", exact: "1.1.0"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.1.0")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXAudioBL.git", exact: "4.2.0"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "4.2.0")
 	],
 	targets: [
 		.target(name: "GXAudioUIWrapper",
 				dependencies: [
 					"GXAudioUI",
-					.product(name: "GXAudioBL", package: "GXAudioBL", condition: .when(platforms: [.iOS])),
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS]))
+					.product(name: "GXAudioBL", package: "GXAudioBL", condition: .when(platforms: [.iOS, .visionOS])),
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .visionOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXAudioUI",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXAudioUI-1.1.0.xcframework.zip",
-			checksum: "312a9d2877c039c7688d1dbd331d82ac48779bf27b51b7cdc06aac192ceb0683"
+			url: "https://pkgs.genexus.dev/iOS/releases/GXAudioUI-4.2.0.xcframework.zip",
+			checksum: "8eddb14bed2dca03c6eddb5ee7e5f91d5e8c78f81caa2562e00d4b71cf26f048"
 		)
 	]
 )
